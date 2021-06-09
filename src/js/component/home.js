b11d58bd4e89e7d15c0722e03d888b602bfa7a81
cavadasA <antonio.cavadas.gu@gmail.com>
2021-06-09 17:19:18 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { Navbar } from "./navbar";
+import { Jumbotron } from "./jumbotron";
+import { Card } from "./card";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,19 +9,28 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<Navbar />
+			<Jumbotron />
+			<div className="row justify-content-around ml-5">
+				<div className="col-3">
+					<Card />
+				</div>
+				<div className="col-3">
+					<Card />
+				</div>
+				<div className="col-3">
+					<Card />
+				</div>
+				<div className="col-3">
+					<Card />
+				</div>
+			</div>
+			<div className="bg-dark py-5 mt-5">
+				<h4 className="text-center text-white">
+					Copyright your website
+				</h4>
+			</div>
+		</>
 	);
 }
---
+++ src/js/component/jumbotron.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export function Jumbotron() {
+	return (
+		<div className="jumbotron mt-4 mx-5">
+			<h1 className="display-4">A warm welcome!</h1>
+			<p className="lead">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+				eiusmod tempor incididunt ut labore et dolore magna aliqua.
+			</p>
+			<a className="btn btn-primary btn-lg" href="#" role="button">
+				Call to action
+			</a>
+		</div>
+	);
+}
